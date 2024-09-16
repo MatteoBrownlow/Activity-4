5da4c107a0ed246714be70d2c3af387670902d6b
MatteoBrownlow <114264680+MatteoBrownlow@users.noreply.github.com>
2024-09-15 19:47:04 -0700

    Update activity4.cpp

--- activity4.cpp
+++ activity4.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(){
   cout << "Course:     Students:  " << endl;
   cout << "C++" << setw(21) << "50" << endl;
-  
+  cout << "Javascript" << setw(15) << "100" << endl;
   
   return 0;
 }
